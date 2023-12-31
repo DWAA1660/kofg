89dc4f92f104a6b3638ca4e94de41f05f4db1f6d
DWAA1660 <dwatnip123@gmail.com>
2023-12-31 14:19:53 -0700

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/complete')
+def complete():
+    return render_template("complete.html")
+
+
+@app.route('/ministries')
+def ministries():
+    return render_template("ministries.html")
+
+@app.route('/programs')
+def programs():
+    return render_template("programs.html")
+
+@app.route('/mobil')
+def mobil():
+    return render_template("mobil.html")
+
+
+app.run("0.0.0.0", port=5000, debug=True)
